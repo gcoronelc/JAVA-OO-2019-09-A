513e4dc5ba0d513e2c99b9026930ad7a3100a297
Gustavo Coronel <gcoronelc@gmail.com>
2019-09-17 09:17:59 -0500

    Clase 02

---
+++ Clase02/AprendiendoJava/src/dto/ProductoDto.java
@@ -0,0 +1,53 @@
+package dto;
+
+public class ProductoDto {
+
+	private int id;
+	private String nombre;
+	private int stock;
+	private double precio;
+	private boolean activo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+	
+	
+	
+}
